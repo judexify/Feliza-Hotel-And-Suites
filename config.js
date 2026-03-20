6882d8fcff99d7501577ce49d6e2b8c19ac7b435
Judexify <olukojuoluwadunsi12@gmail.com>
2026-03-20 23:57:59 +0100

    fix fetched data

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  CSV_FOOD:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSTyDWP86jlo1DWydlBTh5mYpz2LwqQZCJY76Ol8BBETnEUWy3qrB_thU2U_Zj2ie1SAParpPkDkesC/pub?gid=73753195&single=true&output=csv",
+  CSV_DRINKS:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vSTyDWP86jlo1DWydlBTh5mYpz2LwqQZCJY76Ol8BBETnEUWy3qrB_thU2U_Zj2ie1SAParpPkDkesC/pub?gid=1006818673&single=true&output=csv",
+};
